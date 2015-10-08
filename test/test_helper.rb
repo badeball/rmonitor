d21bad92ec9e0d36413e421344cb13b8b559b1c4
Jonas Amundsen <jonasba@gmail.com>
2015-10-08 15:19:21 +0200

    Add a minitest matcher for asserting subsets

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,11 @@ require "minitest/reporters"
 DEFAULT_CONFIG_PATH = File.expand_path "~/.config/rmonitor/config.rb"
 
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+
+module Minitest::Assertions
+  def assert_subset_of(collection, subset)
+    subset.each do |element|
+      assert_includes collection, element
+    end
+  end
+end
