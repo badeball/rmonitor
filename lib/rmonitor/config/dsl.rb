07720d640fc9c24109291f5269775cbd49c92b4f
Jonas Amundsen <jonasba@gmail.com>
2015-10-09 17:24:51 +0200

    :only_if and :not_if methods are now attached

--- lib/rmonitor/config/dsl.rb
+++ lib/rmonitor/config/dsl.rb
@@ -11,6 +11,14 @@ class RMonitor
 
       def profile(name, options = {}, &block)
         @profiles << RMonitor::Profile::Builder.define(name, options, &block)
+
+        if @profiles.last.has_key?(:only_if)
+          @profiles.last[:only_if] = method(@profiles.last[:only_if])
+        end
+
+        if @profiles.last.has_key?(:not_if)
+          @profiles.last[:not_if] = method(@profiles.last[:not_if])
+        end
       end
     end
   end
--- test/config/builder_test.rb
+++ test/config/builder_test.rb
@@ -32,6 +32,20 @@ describe RMonitor::Config::Builder do
       assert_equal expected, actual
     end
 
+    it "should attach methods to the :only_if :not_if options" do
+      profiles = RMonitor::Config::Builder.define do
+        def foo; end
+        def bar; end
+
+        profile "default", :only_if => :foo, :not_if => :bar do
+          device "eDP1", :mode => "1920x1080"
+        end
+      end
+
+      assert_kind_of Method, profiles.first[:only_if]
+      assert_kind_of Method, profiles.first[:not_if]
+    end
+
   end
 
 end
